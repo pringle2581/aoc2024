9ffe8e02f17f4740ac1136c9a2c1ee586cb8228c
pringle2581 <kmjohnstonjr@gmail.com>
2024-12-01 10:19:20 -0500

    day 01

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using aoc2024.solutions;
+
+string input = "../../../input/";
+
+static void PrintResults(int day, string[] results)
+{
+    Console.WriteLine($"Day {day} Part 1: {results[0]}");
+    Console.WriteLine($"Day {day} Part 2: {results[1]}\n");
+}
+
+PrintResults(1, Day01.Solve(File.ReadAllLines(input + "01")));
---
+++ solutions/Day01.cs
@@ -0,0 +1,37 @@
+﻿namespace aoc2024.solutions
+{
+    internal class Day01
+    {
+        static public string[] Solve(string[] input)
+        {
+            int part1 = 0, part2 = 0;
+            List<int> left = [], right = [];
+            Dictionary<int, int> rightdict = [];
+            Parse(input);
+            Solve();
+            return [part1.ToString(), part2.ToString()];
+
+            void Parse(string[] input)
+            {
+                foreach (string line in input)
+                {
+                    string[] num = line.Split("   ");
+                    left.Add(int.Parse(num[0]));
+                    right.Add(int.Parse(num[1]));
+                    rightdict[int.Parse(num[1])] = 1 + (rightdict.TryGetValue(int.Parse(num[1]), out int result) ? result : 0);
+                }
+            }
+
+            void Solve()
+            {
+                left.Sort();
+                right.Sort();
+                for (int i = 0; i < left.Count; i++)
+                {
+                    part1 += Math.Abs(left[i] - right[i]);
+                    part2 += left[i] * (rightdict.TryGetValue(left[i], out int result) ? result : 0);
+                }
+            }
+        }
+    }
+}
