7df7c127f6d33fd65b43e232818bffa2916784a0
pringle2581 <kmjohnstonjr@gmail.com>
2024-12-05 11:40:46 -0500

    day 05

--- Program.cs
+++ Program.cs
@@ -12,3 +12,4 @@ PrintResults("01", Day01.Solve(File.ReadAllLines(input + "01")));
 PrintResults("02", Day02.Solve(File.ReadAllLines(input + "02")));
 PrintResults("03", Day03.Solve(File.ReadAllLines(input + "03")));
 PrintResults("04", Day04.Solve(File.ReadAllLines(input + "04")));
+PrintResults("05", Day05.Solve(File.ReadAllLines(input + "05")));
---
+++ solutions/Day05.cs
@@ -0,0 +1,65 @@
+﻿namespace aoc2024.solutions
+{
+    internal class Day05
+    {
+        static public string[] Solve(string[] input)
+        {
+            int part1 = 0, part2 = 0;
+            List<int[]> rules = [], updates = [];
+            Parse(input);
+            foreach (int[] update in updates)
+            {
+                if (Validate(update))
+                {
+                    part1 += update[update.Length / 2];
+                }
+                else
+                {
+                    part2 += update[update.Length / 2];
+                }
+            }
+            return [part1.ToString(), part2.ToString()];
+
+            void Parse(string[] input)
+            {
+                foreach (string line in input)
+                {
+                    if (line.Contains('|'))
+                    {
+                        rules.Add(line.Split("|").Select(int.Parse).ToArray());
+                    }
+                    else if (line.Contains(','))
+                    {
+                        updates.Add(line.Split(",").Select(int.Parse).ToArray());
+                    }
+                }
+            }
+
+            bool Validate(int[] update)
+            {
+                bool correct = true;
+                bool complete = false;
+                while (!complete) {
+                    bool changed = false;
+                    foreach (int[] rule in rules)
+                    {
+                        if (update.Contains(rule[0]) && update.Contains(rule[1]))
+                        {
+                            if (Array.IndexOf(update, rule[0]) > Array.IndexOf(update, rule[1]))
+                            {
+                                correct = false;
+                                changed = true;
+                                (update[Array.IndexOf(update, rule[0])], update[Array.IndexOf(update, rule[1])]) = (update[Array.IndexOf(update, rule[1])], update[Array.IndexOf(update, rule[0])]);
+                            }
+                        }
+                    }
+                    if (!changed)
+                    {
+                        complete = true;
+                    }
+                }
+                return correct;
+            }
+        }
+    }
+}
